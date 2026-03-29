cdbeaf9a930ea2e3f5a6af8bd0bb27c3e5812223
Ved Bajaj <bajajved21@gmail.com>
2026-03-29 18:51:25 +0530

    Delete backend directory

--- backend/fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('server.js', 'utf8');
-c = c.replace('localhost:3000', 'localhost:5173');
-fs.writeFileSync('server.js', c);
-console.log('Fixed:', c.match(/localhost:\d+/g));
